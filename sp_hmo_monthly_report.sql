a4ae3c3117f0b020ee4f91bbf9e3024bfcc280bd
Francisco Habana <franciscohabana3@gmail.com>
2025-10-27 14:46:35 +0800

    added other sub procedures

--- sp_hmo_monthly_report.sql
+++ sp_hmo_monthly_report.sql
@@ -1,16 +1,32 @@
-CREATE PROCEDURE dbo.sp_hmo_monthly_report
-AS
-BEGIN
-    SET NOCOUNT ON;
-    
-    -- Call  existing sub-procedures 
-    EXEC dbo.sp_m_dspremiumregister;
-    EXEC dbo.sp_m_dscashdisbursement_reg;
-    EXEC dbo.sp_m_dsclaimlistreport;
-	EXEC dbo.sp_m_dsjournalvoucher;
-    
-END
-GO
+CREATE
+OR
+ALTER PROCEDURE dbo.sp_hmo_monthly_report AS BEGIN
+SET
+    NOCOUNT ON;
 
--- Usage:
-EXEC dbo.sp_hmo_monthly_report;
+-- Call existing sub-procedures 
+EXEC dbo.sp_m_dspremiumregister;
+
+EXEC dbo.sp_m_dscashdisbursement_reg;
+
+EXEC dbo.sp_m_dsclaimlistreport;
+
+EXEC dbo.sp_m_dsaccountspayableregister;
+
+EXEC dbo.sp_m_dsagingreport;
+
+EXEC dbo.sp_m_dscashreceiptsregister;
+
+EXEC dbo.sp_m_dsjournalvoucher;
+
+EXEC dbo.sp_m_dslistofcorporatebillings;
+
+EXEC dbo.sp_m_dslistutilmonth;
+
+EXEC dbo.sp_m_dsproductionreport;
+
+EXEC dbo.sp_m_dsrenewallist;
+
+EXEC dbo.sp_m_dssummaryutilreport;
+
+END GO
